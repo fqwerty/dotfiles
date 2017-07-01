b8db5bc64261210076f486c9b888dcd80bf2fc05
Furkan <oyibo.test@yandex.com>
2017-07-01 22:32:17 +0300

    some modifications

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
-
+mkdir -p ~/.jupyter/custom/ #create dirs if not exists
 ln -s ~/.dotfiles/jupyter/custom.js ~/.jupyter/custom/custom.js #custom settings for jupyter notebook vim integration
+mkdir -p ~/.config/nvim/ #create dirs if not exists
 ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim #create a link for init.vim to where it should be
 ln -s ~/.dotfiles/nvim/init.vim ~/.vimrc #symlink init.vim to home folder for easy editing
 ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf #symlink .tmux.conf to home
 ln -s ~/.dotfiles/.bashrc ~/.bashrc #symlink .bashrc to home
 ln -s ~/.dotfiles/.profile ~/.profile #symlink .profile to home
 ln -s ~/.dotfiles/IntelliJ_IDEA/.ideavimrc ~/.ideavimrc #symlink .ideavimrc to home
+
+
 
